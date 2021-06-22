75e6eac764f846a0d72fafe7f297d5bf0f2dadfd
arpitzn <82138001+arpitzn@users.noreply.github.com>
2021-06-22 12:06:46 +0530

    Update sender.py

--- sender.py
+++ sender.py
@@ -16,20 +16,3 @@ while 2>1:
 
     print("your message is sent")
 
-# import socket 
-# import time
-# # import socket module /library to create 
-# s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-# # what is my receiver address
-# recv_addr=("127.0.0.1",9999)
-# # Now i want to send message
-# while 2 > 1 :
-#     user_data=input("enter your message :- ")
-#     # converting data into ascii code
-#     newdata=user_data.encode('ascii')
-#     # now you can send data 
-#     s.sendto(newdata, recv_addr)
-#     time.sleep(2)
-#     print("your message has been sent..")
-#     # now waiting for reply 
-#     print(s.recvfrom(10))
